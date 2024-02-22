a8dec9a089e60e240a557b45174c1b20d75aa4ef
neres.hugo <neres.hugo@eb.mil.br>
2024-02-22 16:46:14 -0300

    validando login

--- cypress/e2e/gui/login.cy.js
+++ cypress/e2e/gui/login.cy.js
@@ -2,5 +2,6 @@ describe('Login', () => {
     it('login com usuário e senha inválidos', () => {
         cy.login()
         cy.url().should('be.equal', `${Cypress.config('baseUrl')}/#/`)
+        cy.title().should('be.equal', 'SPED 3.0')
     })
 })
